b0f1573637f516e6483754ddf12971187c1855d4
Charlie Cheever <ccheever@gmail.com>
2015-04-08 03:04:41 -0700

    Initial commit

---
+++ InvertibleScrollView.js
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2015-present, 650 Industries, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the MIT License
+ *
+ * @flow
+ */
+'use strict';
+
+var React = require('React');
+var ScrollView = require('ScrollView');
+
+var PropTypes = React.PropTypes;
+
+var InvertibleScrollView = React.createClass({
+  propTypes: {...ScrollView.propTypes,
+    inverted: PropTypes.bool,
+  },
+
+  render() {
+    if (this.props.inverted) {
+      var invertedChildren = React.Children.map(this.props.children, (child) => {
+        if (child) {
+          // Figure out how to not use a wrapper View when not necessary
+          // For ListViews, we use them because StaticRenderers, are used
+          // for all of children
+          return <View style={styles.inverted}>{child}</View>;
+        } else {
+          return child;
+        }
+      }, this.context);
+      return <ScrollView {...this.props, style: [styles.inverted, this.props.style], children: invertedChildren,} />;
+    } else {
+      return <ScrollView {...this.props} />;
+    }
+  },
+
+});
+
+var styles = StyleSheet.create({
+  inverted: {
+    transformMatrix:
+      [ -1,  0,  0,  0,
+         0, -1,  0,  0,
+         0,  0,  1,  0,
+         0,  0,  0,  1,
+      ],
+  },
+});
+
+module.exports = InvertibleScrollView;
